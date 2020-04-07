7ad19241fa2aec98ce66adf0c545db1858f1667d
Tsungyu Yu <ytyubox@gmail.com>
2020-04-07 16:02:24 +0800

    add get post APIendpoint test

--- Tests/RESTCombineTests/RESTCombineTests.swift
+++ Tests/RESTCombineTests/RESTCombineTests.swift
@@ -2,18 +2,18 @@ import XCTest
 @testable import RESTCombine
 
 final class RESTCombineTests: XCTestCase {
-	private struct SomeBody:Body {
-		var queryDic: [String : String] = ["foo":"bar"]
-		var encoded: Data?
-	}
 	private struct SomeFailure:Error, Decodable {}
+	enum SampleGroup: APINameSpace {
+		static var baseURL: String = "https://someserver.com:8888/"
+		
+		static var baseHeader: [String : String] = ["Referer":"https://someReferer.com"]
+		
+	}
 	
-	func testAPIEndpoint() {
-		enum SampleGroup: APINameSpace {
-			static var baseURL: String = "https://someserver.com:8888/"
-			
-			static var baseHeader: [String : String] = ["Referer":"https://someReferer.com"]
-			
+	func testGetAPIEndpoint() {
+		struct SomeBody:Body {
+			var queryDic: [String : String] = ["foo":"bar"]
+			var encoded: Data?
 		}
 		var someAPI = APIEndPoint<SomeBody,String,SomeFailure>(
 			endpoint: "some/path",
@@ -29,8 +29,31 @@ final class RESTCombineTests: XCTestCase {
 		XCTAssertEqual(request.allHTTPHeaderFields, ["Referer":"https://someReferer.com"])
 		XCTAssertEqual(request.httpMethod, "GET")
 	}
+	func testPostAPIEndpoint() {
+		struct SomeBody:Body, Codable {
+			var text = "SomeText"
+			var queryDic: [String : String] = ["foo":"bar"]
+		}
+		var someAPI = APIEndPoint<SomeBody,String,SomeFailure>(
+			endpoint: "some/path",
+			httpMethod: .POST,
+			contentType: .json,
+			apiList: SampleGroup.self)
+		someAPI.body = SomeBody()
+		let request = someAPI.request
+		XCTAssertEqual(request.url?.host, "someserver.com")
+		XCTAssertEqual(request.url?.port, 8888)
+		XCTAssertEqual(request.url?.path, "/some/path")
+		XCTAssertEqual(request.url?.query, "foo=bar")
+		XCTAssertEqual(request.allHTTPHeaderFields, ["Referer":"https://someReferer.com","Content-Type": "application/json; charset=UTF-8"])
+		XCTAssertEqual(request.httpMethod, "POST")
+		XCTAssertNotNil(request.httpBody)
+		XCTAssertNoThrow(try JSONDecoder().decode(SomeBody.self, from: request.httpBody!), "request's body is not body")
+	}
+	
 	
 	static var allTests = [
-		("testExample", testAPIEndpoint),
+		("testExample", testGetAPIEndpoint),
+		("testPostAPIEndpoint",testPostAPIEndpoint),
 	]
 }
